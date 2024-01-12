1f82232775e30b16c6260b225bc54196bc413228
sachin-dagar <145598037+sachin-dagar@users.noreply.github.com>
2024-01-12 17:04:49 +0530

    banner wave

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,9 +8,24 @@ function Banner() {
   return (
     <>
       <div
-        className="bg-cover bg-center h-[450px] text-white py-24 px-10 object-fill"
+        className="bg-cover  h-[523px] text-white py-24 px-10 object-fill"
         style={{ backgroundImage: `url(${bannerImage})` }}
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1440"
+          height="88"
+          viewBox="0 0 1440 88"
+          fill="none"
+          className="absolute bottom-0 left-0 w-full top-[518px]"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M0 0L60 15C120 29 240 59 360 73C480 88 600 88 720 81C840 73 960 59 1080 59C1200 59 1320 73 1380 81L1440 88H1380C1320 88 1200 88 1080 88C960 88 840 88 720 88C600 88 480 88 360 88C240 88 120 88 60 88H0V0Z"
+            fill="#FAFAFA"
+          />
+        </svg>
         <div className="md:w-1/2">
           <p className="text-5xl  font-light text-[#FAFAFA] mb-2">
             WE IMPROVE YOUR
@@ -27,11 +42,10 @@ function Banner() {
           </a>
         </div>
       </div>
-      <div className="bg-gray-100 p-4 flex justify-between space-x-4">
+      <div className="bg-[#FAFAFA] p-4 flex justify-between pt-14 space-x-4">
         <div className="w-1/3 bg-white shadow-md p-4 rounded-lg flex flex-col justify-between border-b-4 border-[#009688]">
           <div className="flex flex-col items-center mb-4">
             <img src={handIcon} alt="Hand Icon" className="h-8 w-8 mb-2" />{" "}
-         
             <h2 className="text-lg font-semibold text-center  border-b-2 border-[#4C4C4C]">
               Consumer Insights
             </h2>
@@ -44,7 +58,6 @@ function Banner() {
         <div className="w-1/3 bg-white shadow-md p-4 rounded-lg flex flex-col justify-between border-b-4 border-[#009688]">
           <div className="flex flex-col items-center mb-4">
             <img src={speakerIcon} alt="Hand Icon" className="h-8 w-8 mb-2" />{" "}
-       
             <h2 className="text-lg font-semibold text-center  border-b-2 border-[#4C4C4C]">
               Emerging Ideas
             </h2>
@@ -57,7 +70,6 @@ function Banner() {
         <div className="w-1/3 bg-white shadow-md p-4 rounded-lg flex flex-col justify-between border-b-4 border-[#009688]">
           <div className="flex flex-col items-center mb-4">
             <img src={goalIcon} alt="Hand Icon" className="h-8 w-8 mb-2" />{" "}
-          
             <h2 className="text-lg font-semibold text-center  border-b-2 border-[#4C4C4C]">
               Marketing Goals
             </h2>
